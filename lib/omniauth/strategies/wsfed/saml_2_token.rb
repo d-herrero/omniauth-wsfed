20bf7bb93c714955a9b957591cdec2f3bb10f2bd
d-herrero <daniel.herrero.101@gmail.com>
2018-04-20 15:42:52 +0200

    SAML2Token changed to look in more than one path when looking for audience, issuer and claims.

--- lib/omniauth/strategies/wsfed/saml_2_token.rb
+++ lib/omniauth/strategies/wsfed/saml_2_token.rb
@@ -2,7 +2,6 @@ module OmniAuth
   module Strategies
     class WSFed
       class SAML2Token
-
         attr_accessor :document
 
         def initialize(document)
@@ -10,18 +9,39 @@ module OmniAuth
         end
 
         def audience
-          applies_to = REXML::XPath.first(document, '//t:RequestSecurityTokenResponse/wsp:AppliesTo', { 't' => WS_TRUST, 'wsp' => WS_POLICY })
-          REXML::XPath.first(applies_to, '//EndpointReference/Address').text
+          # We capture any possible error to prevent check problems.
+          begin
+            # We look in "//t:RequestSecurityTokenResponse/wsp:AppliesTo -> //EndpointReference/Address" first.
+            applies_to = REXML::XPath.first(document, '//t:RequestSecurityTokenResponse/wsp:AppliesTo', { 't' => WS_TRUST, 'wsp' => WS_POLICY })
+            result     = applies_to.present? ? REXML::XPath.first(applies_to, '//EndpointReference/Address') : nil
+
+            # If nothing is found, we look in "//samlp:Response/saml:Assertion/saml:Conditions/saml:AudienceRestriction/saml:Audience" instead.
+            result     = REXML::XPath.first(document, '//samlp:Response/saml:Assertion/saml:Conditions/saml:AudienceRestriction/saml:Audience') unless result
+
+            result ? result.text : nil
+          rescue
+            nil
+          end
         end
 
         def issuer
-          REXML::XPath.first(document, '//Assertion/Issuer').text
+          # As we did in #audience, we look in several paths and capture errors.
+          begin
+            result = REXML::XPath.first(document, '//Assertion/Issuer')
+            result = REXML::XPath.first(document, '//samlp:Response/saml:Issuer') unless result
+
+            result ? result.text : nil
+          rescue
+            nil
+          end
         end
 
         def claims
+          # Same as #audience and #issuer.
           stmt_element = REXML::XPath.first(document, '//Assertion/AttributeStatement')
+          stmt_element = REXML::XPath.first(document, '//samlp:Response/saml:Assertion/saml:AttributeStatement') unless stmt_element
 
-          return {} if stmt_element.nil?
+          return {} unless stmt_element
 
           {}.tap do |result|
             stmt_element.elements.each do |attr_element|
@@ -38,7 +58,6 @@ module OmniAuth
             end
           end
         end
-
       end
     end
   end
